039c8d6aa7564b36fe36c7adeb69f53acea76d0f
Pavlo Svirin <pavlosvirin@130-199-21-196.dhcp.bnl.gov>
2017-11-21 18:33:52 -0500

    Added solarized color scheme installation and info prints

--- init-vimrc.sh
+++ init-vimrc.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 
 if [ -f ~/.vimrc ]; then
-	exit 1
+    echo Please backup your .vimrc first
+	  exit 1
 fi
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# installing solarized color scheme
+echo installing solarized color scheme...
+
+SOLARIZEDDIR=$(mktemp -d)
+
+if [ -z ${SOLARIZEDDIR} ]; then
+    echo Failed to create temporary directory
+    exit 1
+fi
+
+git clone git://github.com/altercation/vim-colors-solarized.git $SOLARIZEDDIR/vim-colors-solarized
+
+if [[ $? -ne 0 ]]; then
+    echo Failed to clone solarized git repository
+fi
+
+if [[ ! -d ~/.vim/colors ]]; then
+    mkdir -p ~/.vim/colors
+fi
+
+mv $SOLARIZEDDIR/vim-colors-solarized/colors/solarized.vim ~/.vim/colors/
+
+rm -rf $SOLARIZEDDIR
+
